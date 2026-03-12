47ce49e538974f358e1a985bd8c4f39e1f311d63
NicolasleonardoDev <nicoleoppessoa@gmail.com>
2026-03-12 01:29:13 -0300

    first commit

---
+++ Primeiro Projeto.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+
+int main(){
+    
+    int idade = 0;
+    float media = 0.0;
+    float nota = 0;
+    char nome [30] = "";
+    
+    printf("Coloque sua idade: ");
+    scanf("%d", &idade);
+      
+
+    printf("Coloque sua media: ");
+    scanf("%f", &media);
+    
+
+    printf("Coloque a sua nota: ");
+    scanf(" %f", &nota);
+
+
+
+
+    printf("%d\n", idade); 
+    printf("%.2f\n", media);
+    printf("%.1f\n", nota);
+    printf("%s\n", nome);
+
+    return 0;
+
+}
